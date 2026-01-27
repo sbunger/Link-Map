a27061d92398c859b01c3cb7a945ba94abf4d261
Silas Unger <silasburtonunger@gmail.com>
2026-01-26 23:02:21 -0800

    fixed my horrible mistakes

--- public/script.js
+++ public/script.js
@@ -1,8 +1,19 @@
+let map;
+let routeLine;
+
+function initMap() {
+    map = L.map('map').setView([47.6, -122.33], 11);
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"', {
+        attribution: '&copy; CARTO'
+    }).addTo(map);
+}
+
 async function loadArrivals() {
     const res = await fetch("/api/arrivals");
     const data = await res.json();
 
-    const { stopName, arrivals } = data;
+    const { stopName, arrivals, shape } = data;
 
     document.getElementById("stopName").textContent = `Stop: ${stopName}`;
 
@@ -12,7 +23,9 @@ async function loadArrivals() {
     arrivals.forEach(a => {
         const li = document.createElement("li");
 
-        const minutes = Math.round((a.predictedArrivalTime - Date.now()) / 60000)
+        const arrivalTime = a.predictedArrivalTime || a.scheduledArrivalTime;
+
+        const minutes = Math.round((arrivalTime - Date.now()) / 60000)
 
         if (minutes < 0) {
             li.textContent = `${a.routeShortName} arrived ${(minutes * -1)} minutes ago.`
@@ -25,7 +38,22 @@ async function loadArrivals() {
 
         list.appendChild(li);
     });
+
+    //console.log(shape.data.entry);
+    const coordinates = polyline.decode(shape.data.entry.points);
+
+    routeLine = L.polyline(coordinates, {
+        color: 'red',
+        weight: 4,
+        opacity: 0.8
+    }).addTo(map);
+
+    //map.fitBounds(routeLine.getBounds());
 }
 
-loadArrivals();
-setInterval(loadArrivals, 30000);
+window.onload = () => {
+    initMap();
+    loadArrivals();
+
+    setInterval(loadArrivals, 30000);
+};
