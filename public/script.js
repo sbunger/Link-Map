b0e6e79e7c302ac74039b00293a978ddee1cd8f7
Silas Unger <silasburtonunger@gmail.com>
2026-01-27 21:46:05 -0800

    route selection, detection of nearby routes

--- public/script.js
+++ public/script.js
@@ -1,12 +1,43 @@
 let map;
 let routeLine;
+let userLocation;
+let routeLines = [];
+let selectedRouteLine;
+
+let defaultLine = {
+    color: "#ff0000",
+    weight: 3,
+    opacity: 0.3
+}
+
+let selectedLine = {
+    color: "#0008ff",
+    weight: 5,
+    opacity: 0.8
+}
 
 function initMap() {
     map = L.map('map').setView([47.6, -122.33], 11);
 
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"', {
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; CARTO'
     }).addTo(map);
+
+    map.on("click", () => {
+        if (selectedRouteLine) {
+            selectedRouteLine.setStyle(defaultLine);
+            selectedRouteLine = null;
+        }
+    });
+
+    navigator.geolocation.getCurrentPosition(async (pos) => {
+        userLocation = pos.coords;
+        updateMap();
+        console.log(userLocation);
+
+        map.setView([userLocation.latitude, userLocation.longitude], 13);
+    });
+
 }
 
 async function loadArrivals() {
@@ -38,17 +69,44 @@ async function loadArrivals() {
 
         list.appendChild(li);
     });
+}
 
-    //console.log(shape.data.entry);
-    const coordinates = polyline.decode(shape.data.entry.points);
+function highlightRoute(line) {
+    routeLines.forEach(l => {
+        l.setStyle(defaultLine);
+    });
 
-    routeLine = L.polyline(coordinates, {
-        color: 'red',
-        weight: 4,
-        opacity: 0.8
-    }).addTo(map);
+    line.setStyle(selectedLine);
 
-    //map.fitBounds(routeLine.getBounds());
+    line.bringToFront();
+    selectedRouteLine = line;
+}
+
+
+async function updateMap() {
+    
+
+    const res = await fetch(
+        `/api/routes-nearby?lat=${userLocation.latitude}&lon=${userLocation.longitude}`
+    );
+
+    const routes = await res.json();
+
+    routes.forEach(r => {
+        const coords = polyline.decode(r.shape.data.entry.points);
+
+        const line = L.polyline(coords, defaultLine)
+        .bindPopup(`${r.name}`)
+        .bindTooltip(`${r.name}`, { sticky: true })
+        .addTo(map);
+
+        line.on("click", async (e) => {
+            e.originalEvent.stopPropagation();
+            highlightRoute(line);
+        });
+
+        routeLines.push(line);
+    });   
 }
 
 window.onload = () => {
