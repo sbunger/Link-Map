7bf2be649b6aeabd8602d6c1e7b343632e4ce5b1
Silas Unger <silasburtonunger@gmail.com>
2026-02-23 21:09:49 -0800

    map refactor complete

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useMemo, useRef } from "react";
 
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
@@ -6,6 +6,8 @@ import "leaflet/dist/leaflet.css";
 import "leaflet.markercluster";
 import 'leaflet.markercluster/dist/MarkerCluster.css';
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+
+import '../styles.css'
 
 export default function MapView({
     darkMode,
@@ -20,27 +22,79 @@ export default function MapView({
     const layers = useRef({});
     const routeLines = useRef([]);
 
-    let stopIcon = L.icon({
-        iconUrl: "/images/stop-icon.png",
-        iconSize: [24, 24],
-        opacity: 0.8,
-    });
+    const hoverTooltipRef = useRef(null);
+    const hoverTimeoutRef = useRef(null);
 
-    let selectedStopIcon = L.icon({
-        iconUrl: '/images/selected-stop-icon.png',
-        iconSize: [34, 34],
-        opacity: 0.8,
-    });
+    function isNearLine(map, latlng, polyline, tolerance = 8) {
+        const point = map.latLngToLayerPoint(latlng);
 
-    const busIcon = L.icon({
-        iconUrl: "/images/bus.png",
-        iconSize: [24, 24],
-    });
+        for (let i = 0; i < polyline.latLngs.length - 1; i++) {
+            const p1 = map.latLngToLayerPoint(polyline.latLngs[i]);
+            const p2 = map.latLngToLayerPoint(polyline.latLngs[i + 1]);
+            if (L.LineUtil.pointToSegmentDistance(point, p1, p2) <= tolerance) return true;
+        }
+        return false;
+    }
 
-    const linkIcon = L.icon({
-        iconUrl: "/images/rail.png",
-        iconSize: [26, 26],
-    });
+    function isLineVisible(line, bounds) {
+        return line.latLngs?.some(ll => bounds.contains(ll));
+    }
+
+    const stopIconLight = useMemo(
+        () =>
+            L.icon({
+                iconUrl: "/images/stop-icon.png",
+                iconSize: [24, 24]
+            }),
+        []
+    );
+
+    const stopIconDark = useMemo(
+        () =>
+            L.icon({
+                iconUrl: "/images/stop-icon-dark.png",
+                iconSize: [24, 24]
+            }),
+        []
+    );
+
+    
+    const selectedStopIconLight = useMemo(
+        () =>
+            L.icon({
+                iconUrl: "/images/selected-stop-icon.png",
+                iconSize: [28, 28]
+            }),
+        []
+    );
+
+    const selectedStopIconDark = useMemo(
+        () =>
+            L.icon({
+                iconUrl: "/images/selected-stop-icon-dark.png",
+                iconSize: [28, 28]
+            }),
+        []
+    );
+
+    const busIcon = useMemo(
+        () =>
+            L.icon({
+                iconUrl: "/images/bus.png",
+                iconSize: [24, 24],
+            }),
+        []
+    );
+
+    const linkIcon = useMemo(
+        () =>
+            L.icon({
+                iconUrl: "/images/rail.png",
+                iconSize: [26, 26],
+            }),
+        []
+    );
+
 
     //map
     useEffect(() => {
@@ -57,10 +111,13 @@ export default function MapView({
         mapInstance.current = map;
 
         map.createPane("vehiclePane");
-        map.getPane("vehiclePane").style.zIndex = 4000;
+        map.getPane("vehiclePane").style.zIndex = 850;
+
+        hoverTooltipRef.current = L.tooltip({ sticky: true, className: "route-tooltip" });
 
         layers.current = {
             stopLayer: L.layerGroup().addTo(map),
+            selectedStopLayer: L.layerGroup().addTo(map),
             routeLayer: L.layerGroup().addTo(map),
             railLayer: L.layerGroup().addTo(map),
             busLayer: L.markerClusterGroup({
@@ -81,6 +138,8 @@ export default function MapView({
             setSelectedStop(null);
         });
     }, []);
+
+
 
     //dark or light mode
     useEffect(() => {
@@ -103,6 +162,8 @@ export default function MapView({
 
     }, [darkMode]);
 
+
+
     //routes
     useEffect(() => {
         const map = mapInstance.current;
@@ -122,18 +183,21 @@ export default function MapView({
                     const coords = routeShape.shape.map(p => [p.shape_pt_lat, p.shape_pt_lon]);
 
                     const isRail = routeShape.route_type === 0 && routeShape.agency_id.startsWith("st_");
-                    const color = isRail ? "#f14377" : routeShape.route_color || "#6FADCA";
+                    const color = isRail ? "#f14377" : getMappedColor(routeShape.route_color);
 
                     const line = L.polyline(coords, {
                         color,
                         weight: 4,
-                        opacity: 0.4
+                        opacity: isRail ? 0.8 : 0.4
                     }).addTo(layers.current.routeLayer);
 
+                    line.routeName = routeShape.name ?? "Unnamed Route"; 
+                    line.latLngs = line.getLatLngs();
                     line.routeId = routeShape.route_id;
-                    line.routeName = routeShape.name || "Unnamed Route";
+                    line.color = color;
                     line.isRail = isRail;
 
+                    layers.current.routeLayer.addLayer(line);
                     routeLines.current.push(line);
                 });
             } catch (err) {
@@ -145,13 +209,36 @@ export default function MapView({
 
     }, []);
 
+    //route colors
+    let colorMap = {};
+    let paletteIndex = 0;
+    const palette = [
+        "#6FADCA", // kc metro
+        "#2f4eb3", // soundtransit i think?
+        "#2f4eb3", // kc water taxi
+        "#2f4eb3", // vashon ferry
+        "#f14377",  // streetcars
+        "#f14377", // rapidride
+    ];
+
+    const getMappedColor = (original) => {
+        if (!original) return "#6FADCA";
+
+        if (!colorMap[original]) {
+            colorMap[original] = palette[paletteIndex % palette.length]
+            paletteIndex++;
+        }
+
+        return colorMap[original];
+    }
+
     //highlight route
     useEffect(() => {
         routeLines.current.forEach(line => {
             if (!highlightedRoute) {
                 line.setStyle({ opacity: line.isRail ? 0.8 : 0.4 });
             } else if (line.routeName === highlightedRoute) {
-                line.setStyle({ opacity: 0.9, weight: 6 });
+                line.setStyle({ opacity: 0.9});
                 
                 const group = L.featureGroup([line]);
                 mapInstance.current.flyToBounds(group.getBounds(), { padding: [50, 50] });
@@ -162,13 +249,61 @@ export default function MapView({
         });
     }, [highlightedRoute]);
 
+    //tooltip
+    useEffect(() => {
+        const map = mapInstance.current;
+        if (!map) return;
+
+        const onMouseMove = (e) => {
+            const tooltip = hoverTooltipRef.current;
+            if (!tooltip) return;
+
+            tooltip.setLatLng(e.latlng);
+
+            if (hoverTimeoutRef.current) return;
+
+            hoverTimeoutRef.current = window.setTimeout(() => {
+                hoverTimeoutRef.current = null;
+
+                const bounds = map.getBounds();
+                const hits = routeLines.current
+                    .filter(line => isLineVisible(line, bounds))
+                    .filter(line => isNearLine(map, e.latlng, line, 8));
+
+                if (hits.length) {
+                    tooltip
+                        .setContent(hits.map(l => l.routeName).join(", "))
+                        .addTo(map);
+                } else {
+                    if (map.hasLayer(tooltip)) map.removeLayer(tooltip);
+                }
+            }, 100);
+        };
+
+        map.on("mousemove", onMouseMove);
+
+        return () => {
+            map.off("mousemove", onMouseMove);
+
+            if (hoverTimeoutRef.current) {
+                clearTimeout(hoverTimeoutRef.current);
+                hoverTimeoutRef.current = null;
+            }
+            const tooltip = hoverTooltipRef.current;
+            if (tooltip && map.hasLayer(tooltip)) map.removeLayer(tooltip);
+        };
+    }, []);
+
     //stops
     useEffect(() => {
         const map = mapInstance.current;
         if (!map) return;
 
-        layers.current.stopLayer.clearLayers();
-        if (!renderStops) return;
+        if (!renderStops) {
+            layers.current.stopLayer.clearLayers();
+            layers.current.selectedStopLayer.clearLayers();
+            return;
+        };
 
         const loadStops = () => {
             const bounds = map.getBounds();
@@ -176,20 +311,45 @@ export default function MapView({
             fetch(`/api/stops-nearby?bbox=${bounds.toBBoxString()}`)
                 .then(res => res.json())
                 .then(stops => {
-                    layers.current.stopLayer.clearLayers();
+                    
 
-                    if (stops.length > 850) return;
+                    const newStopLayer = L.layerGroup();
+                    const newSelectedStopLayer = L.layerGroup();
 
                     stops.forEach(stop => {
-                        const marker = L.marker([stop.lat, stop.lon], { icon: stopIcon });
 
-                        marker.on("click", e => {
-                            L.DomEvent.stopPropagation(e);
-                            setSelectedStop(stop);
-                        });
+                        //clean up ST data
+                        if ((stop.stop_id.includes("LS") || stop.stop_id.includes("C") || stop.name.toLowerCase().includes("entrance") || stop.name.toLowerCase().includes("bay"))  && !(stop.agency == "1")) return;
+                        if ((stop.stop_id.length === 3 || stop.stop_id.length === 4) && !(stop.agency == "1")) return;
+                        if ((stop.name.includes("&")) && !(stop.agency == "1")) return;
 
-                        layers.current.stopLayer.addLayer(marker);
+
+                        if (selectedStop && (selectedStop.stop_id === stop.stop_id)) {
+                            const marker = L.marker([stop.lat, stop.lon], { icon: darkMode ? selectedStopIconDark : selectedStopIconLight, opacity: 0.7 });
+                            marker.addTo(newSelectedStopLayer);
+                        } else {
+                            if (stops.length > 850) {
+                                layers.current.stopLayer.clearLayers();
+                                layers.current.selectedStopLayer.clearLayers();
+                                return;
+                            };
+
+                            const marker = L.marker([stop.lat, stop.lon], { icon: darkMode ? stopIconDark : stopIconLight, opacity: 0.7 });
+
+                            marker.on("click", e => {
+                                L.DomEvent.stopPropagation(e);
+                                setSelectedStop(stop);
+                            });
+
+                            marker.addTo(newStopLayer);
+                        }
                     });
+
+                    layers.current.stopLayer.clearLayers();
+                    layers.current.selectedStopLayer.clearLayers();
+
+                    layers.current.stopLayer.addLayer(newStopLayer);
+                    layers.current.selectedStopLayer.addLayer(newSelectedStopLayer);
                 })
                 .catch(err => console.error("Stop load error:", err));
         };
@@ -202,7 +362,9 @@ export default function MapView({
             map.off("moveend", loadStops);
         };
 
-    }, [renderStops]);
+    }, [renderStops, darkMode, selectedStop]);
+
+
 
     //vehicles
     useEffect(() => {
@@ -217,11 +379,16 @@ export default function MapView({
                 const newBusLayer = L.layerGroup();
                 const newRailLayer = L.layerGroup();
 
+                const bounds = map.getBounds();
+
                 vehicles.forEach(v => {
                     if (!v.lat || !v.lon) return;
-                    const marker = L.marker([v.lat, v.lon], {
+                    const latlng = L.latLng(v.lat, v.lon);
+
+                    const marker = L.marker(latlng, {
                         icon: (v.type === 0) ? linkIcon : busIcon,
                         pane: 'vehiclePane',
+                        opacity: 0.85,
                         interactive: false
                     });
 
@@ -249,6 +416,8 @@ export default function MapView({
         const interval = setInterval(loadVehicles, 15000);
         return () => clearInterval(interval);
     }, [renderVehicles]);
+
+
 
     return <div ref={mapRef} style={{ height: "100vh", width: "100vw" }} />;
 }
